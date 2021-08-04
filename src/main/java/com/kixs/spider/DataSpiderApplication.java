ca41dbc648f7ea32b398cd01aeb2e40a8228a27c
kixs <emp_kixs@163.com>
2021-08-04 11:43:20 +0800

    init

---
+++ src/main/java/com/kixs/spider/DataSpiderApplication.java
@@ -0,0 +1,13 @@
+package com.kixs.spider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataSpiderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataSpiderApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/kixs/spider/poetry/PoetryAuthorSpider.java
@@ -0,0 +1,13 @@
+package com.kixs.spider.poetry;
+
+/**
+ * 诗词作者信息爬取
+ *
+ * @author wangbing
+ * @version v1.0.0
+ * @since 2021/8/4 11:34
+ */
+public class PoetryAuthorSpider {
+
+
+}
